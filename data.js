71a86a65f0f62ee83264a1dc17a8b530501eae01
FangKGM123 <manhy2077@gmail.com>
2026-05-05 16:35:24 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    question: "cao atiso có tác dụng gì",
+    answer: "Cao Atiso giúp thanh nhiệt, giải độc gan",
+  },
+  {
+    question: "tôi bị nóng gan nên dùng gì",
+    answer: "Bạn nên dùng Cao Atiso",
+  },
+  {
+    question: "cao hà thủ ô có tác dụng gì",
+    answer: "Cao Hà Thủ Ô giúp bổ máu và đen tóc",
+  },
+  {
+    question: "tóc bạc sớm nên dùng gì",
+    answer: "Bạn nên dùng Cao Hà Thủ Ô",
+  },
+  {
+    question: "cao hoa ban có tác dụng gì",
+    answer: "Cao Hoa Ban giúp thanh nhiệt, giải độc gan",
+  },
+  {
+    question: "tôi bị nóng gan nên dùng gì",
+    answer: "Bạn nên dùng Cao Hoa Ban",
+  },
+  {
+    question: "giá của cao hoa ban là bao nhiêu",
+    answer: "Giá của Cao Hoa Ban là 350.000 VNĐ",
+  },
+];
